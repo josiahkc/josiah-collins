44bccc02a11fc683a8d0af17b606e1d7c3b8bca9
josiahkc <josiahkc@gmail.com>
2021-09-10 11:25:39 -0700

    initial commit

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Josiah's Blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">Projects</a>
+                <a href="">Resume</a>
+                <a href="">Contact</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
