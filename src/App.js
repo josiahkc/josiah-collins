d6bbe419f6ed664e3f5a7607687ba40c3643fee6
Collins <jcolli29@calstatela.edu>
2021-10-26 10:24:29 -0700

    fucked it all up

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 // import logo from './logo.svg';f
 import Navbar from './Navbar';
 import Home from './Home';
+import { useState } from 'react';
 // import CV from './CV';
 
 
 
-
 function App() {
+
+const [activePage, setActivePage] = useState('Home');
+
+const handleClick = () => {
+    switch (button) {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+  }
   
   return (
     <div className="App">
       <Navbar />
       <div className="content">
-        <Home />
+        
       </div>
     </div> 
   );
--- src/Home.js
+++ src/Home.js
@@ -5,32 +5,20 @@ import CV from './CV';
 
 const Home = () => {
     
-    const [activePage, setActivePage] = useState('Home');
-
-    const handleClick = () => {
-        setActivePage('Projects');
-    }
 
     const[jobs, setJobs] = useState([
-        {title: 'Ass Cleaner', description: 'I clean asses for money', id: 1},
-        {title: 'Cock Boi MC Fuckface', description: 'The name says it all', id: 2}
+        {title: 'Job 1', description: 'Job desctription', id: 1},
+        {title: 'CBMFF', description: 'bruh', id: 2}
     ]);
+
+    
 
     return ( 
         <div className="home">
             <h1>{ activePage }</h1>
             <button onClick={handleClick}>Click Me</button>
             
-            <p className='description'>
-            Curabitur felis nunc, elementum in erat vel, maximus aliquam magna. Nam eget dapibus purus, quis tempor nisl. Vivamus aliquet cursus ex, eget convallis tortor vestibulum vel. Nullam quis magna sodales, aliquet est egestas, ullamcorper justo. Quisque mauris mi, tincidunt quis elit quis, placerat ultricies magna. Fusce volutpat malesuada orci ut laoreet. Pellentesque iaculis enim sed magna venenatis.
-            </p>
-
-            <CV jobs={jobs}/>
-        
-
-            {/* <div className="mainImage">
-
-            </div> */}
+            
         </div>
      );
 }
