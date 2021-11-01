23c0d4ac9a8a7a02739be3a18faf674df3622bfb
Collins <jcolli29@calstatela.edu>
2021-11-01 07:01:38 -0700

    fixing git issues, remember to push your fucing code

--- src/Home.js
+++ src/Home.js
@@ -5,11 +5,6 @@ import CV from './CV';
 
 const Home = () => {
     
-<<<<<<< HEAD
-
-=======
-  
->>>>>>> a4232e19af6eae3ebb6f5519956634ce09e96572
     const[jobs, setJobs] = useState([
         {title: 'Job 1', description: 'Job desctription', id: 1},
         {title: 'CBMFF', description: 'bruh', id: 2}
